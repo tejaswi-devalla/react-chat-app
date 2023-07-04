c772b250b2b73048eec46d32581629bc6c2cd696
tejaswi <tejaswidevalla12@gmail.com>
2023-07-04 13:36:27 +0530

    likes and mentions

--- src/components/ChatHome/index.js
+++ src/components/ChatHome/index.js
@@ -1,3 +1,4 @@
+import Mentions from "rc-mentions";
 import { v4 as uuidv4 } from "uuid";
 import InputEmoji from "react-input-emoji";
 import { Component } from "react";
@@ -26,12 +27,13 @@ class ChatHome extends Component {
     profile: [],
     hours: 0,
     minutes: 0,
+    mentionText: "",
   };
   onEnterInputText = (text) => {
     this.setState({ inputText: text });
   };
   sendMessage = () => {
-    const { inputText } = this.state;
+    const { inputText, mentionText } = this.state;
     const user_list = ["Alan", "Bob", "Carol", "Dean", "Elin"];
     const randomNames = Math.floor(Math.random() * user_list.length);
     const filterNames = user_list[randomNames];
@@ -40,6 +42,7 @@ class ChatHome extends Component {
     const date = new Date();
     this.setState((prevState) => ({
       inputText: "",
+      mentionText: "",
       profile: [
         ...prevState.profile,
         {
@@ -47,7 +50,7 @@ class ChatHome extends Component {
           fullName: filterNames,
           name: filterNames.slice(0, 2),
           profileColor: filterColor,
-          inputText: inputText,
+          inputText: mentionText + inputText,
         },
       ],
       hours: date.getHours(),
@@ -57,16 +60,24 @@ class ChatHome extends Component {
 
   increaseCount = (id) => {
     const { profile } = this.state;
-    this.likesCount[id] = (this.likesCount[id] || 0) + 1;
-    this.forceUpdate();
-    const getid = profile.map((eachItem) => eachItem.id === id);
-    const index = getid.indexOf(true);
-    profile[index].count = this.likesCount[id] || 0;
+    if ((this.likesCount[id] || 0) < 5) {
+      this.likesCount[id] = (this.likesCount[id] || 0) + 1;
+      this.forceUpdate();
+      const getid = profile.map((eachItem) => eachItem.id === id);
+      const index = getid.indexOf(true);
+      profile[index].count = this.likesCount[id] || 0;
+    }
+  };
+
+  onEnterMentionText = (text) => {
+    this.setState({ mentionText: text });
   };
 
   render() {
-    const { inputText, profile, hours, minutes } = this.state;
+    const { inputText, profile, hours, minutes, mentionText } = this.state;
     const user_list = ["Alan", "Bob", "Carol", "Dean", "Elin"];
+    const { Option } = Mentions;
+    console.log(mentionText);
     return (
       <div className="chat-home-container">
         <div className="header">
@@ -94,6 +105,24 @@ class ChatHome extends Component {
             ))}
           </div>
           <div className="input-container">
+            <Mentions
+              onChange={this.onEnterMentionText}
+              placeholder="Add Mentions"
+              placement="top"
+              direction="rtl"
+              split=" "
+              style={{ color: "blue" }}
+            >
+              {user_list.map((eachName) => (
+                <Option
+                  key={eachName}
+                  value={eachName}
+                  className="mention-inputs"
+                >
+                  {eachName}
+                </Option>
+              ))}
+            </Mentions>
             <InputEmoji
               value={inputText}
               onChange={this.onEnterInputText}
@@ -104,7 +133,7 @@ class ChatHome extends Component {
               maxLength={50}
               borderColor="black"
               onEnter={this.sendMessage}
-            />
+            ></InputEmoji>
             <button
               type="button"
               className="send-btn"
